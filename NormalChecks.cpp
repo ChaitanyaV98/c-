21f33766e0124fbe30428830fcefef405ae3472f
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-06 17:51:59 +0530

    added preincrement functionality

--- NormalChecks.cpp
+++ NormalChecks.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 int main(){
     int a= 10;
-    //first assigns and then updates
+    //post increment: first assigns and then updates
     int b=a++;
     cout<<"Values of a " << a << " Value of b "<<b << endl;
+
+    //pre increment : first updates happen and then assigns
+    int c = 2;
+    int d = --c; // c=c-1 and then d=c
+    cout<<"Values of c " << c << " Value of d "<< d << endl;
+
+
+
     return 0;
 }
