da4d4c42b71e4862fd6ef3197191635653bfccca
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-06 15:55:42 +0530

    my first code sum of taking input and showing output

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout <<"enter val a" <<endl;
+    cin>>a;
+    cout <<"enter val b" <<endl;
+    cin>>b;
+int sum = a+b;
+cout<<"sum value "<<sum<<endl;y
+return 0;
+
+}
